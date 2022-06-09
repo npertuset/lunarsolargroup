4728cbdca9fd2fe9f15d23bca9e21f242fe65917
Nathan Pertuset <nathanpertuset@gmail.com>
2022-06-09 14:29:19 -0600

    Fixing menu

--- scripts/Components/Menu.js
+++ scripts/Components/Menu.js
@@ -1,27 +1,38 @@
 import React from "react";
 import styled from "styled-components";
-import MenuClosed from "./MenuClosed";
 
-const StyledButton = styled.div`
-  // display: none;
+import MenuClosed from "./MenuClosed";
+import MenuOpen from "./MenuOpen";
+
+const mobileBreakpoint = "1000px";
+
+const MenuWrapper = styled.div`
   position: absolute;
   top: 0;
   left: 0;
   height: 100vh;
   width: 50%;
   background-color: white;
+  padding: 8px 0 0 15px;
+
+  @media (max-width: ${mobileBreakpoint}) {
+    width: 100%;
+  }
 `;
 export const Menu = () => {
-  const [menuOpen, setLiked] = React.useState(false);
+  const [menuStatusOpen, setMenuStatusOpen] = React.useState(false);
 
   return (
     <>
-      {menuOpen ? (
-        <StyledButton>
-          <div onClick={() => setLiked(!menuOpen)}>Close</div>
-        </StyledButton>
+      {menuStatusOpen ? (
+        <MenuWrapper>
+          <div>
+            <MenuOpen onChildClick={() => setMenuStatusOpen(!menuStatusOpen)} />
+          </div>
+          <div>Dream Light High Rohode Kin Spritz</div>
+        </MenuWrapper>
       ) : null}
-      <MenuClosed onChildClick={() => setLiked(!menuOpen)} />
+      <MenuClosed onChildClick={() => setMenuStatusOpen(!menuStatusOpen)} />
     </>
   );
 };
